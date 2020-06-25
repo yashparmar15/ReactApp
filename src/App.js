8391cc1f3d4f3e2c7677d0e56331e97667e05228
Yash Parmar <yashparmar15700@gmail.com>
2020-06-25 18:37:01 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React , {Component} from 'react';
 
-import Aux from './hoc/Aux';
+
 
 import AppBuilder from './containers/AppBuilder/AppBuilder';
 
 class App extends Component{
     render(){
       return(
-        <Aux>
-            <AppBuilder />
-        </Aux>
+        <AppBuilder />
+
       );
     }
 }
